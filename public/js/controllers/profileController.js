2f8227d80d667f1dc410098beb2fc779df65f814
saveenchad <sachadal@ucsd.edu>
2015-04-30 23:01:53 -0700

    Added placeholders, checks, crosses

---
+++ public/js/controllers/profileController.js
@@ -0,0 +1,14 @@
+/*
+ * profileController.js
+ * Controls the view when the user is logged in and viewing their profile
+ */
+ 
+//Global Variables//
+/******************/
+//Link to Parse database - accepts application_ID, JavaScript_Key
+Parse.initialize( "t5hvXf3wJOYnL3MMIffsemMdhLM7f4brACcf0eBa", "UhqQaEDIEQr6cxhO8XS4Fl8BcGU4ir9jL9To7PVO" );
+var currentUser = Parse.User.current();
+
+app.controller('profileController', ['$scope', function($scope) {
+    
+}]);
