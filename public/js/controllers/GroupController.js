e4be065159a4101846fd1cc9073fdabb4bda18bd
jdeon <jmdeon@gmail.com>
2015-05-05 15:13:27 -0700

    merge Home into ProfileController, add headers for info

--- public/js/controllers/GroupController.js
+++ public/js/controllers/GroupController.js
@@ -1,24 +1,39 @@
 /*
- * GroupController.js
- * Description: This controller will be used to control the group view.
+ * FileName: GroupController.js
+ * Authors: Joe d'Eon (if you edit this file, add your name to this list)
+ * Description: This controller will be used to control the view of a single
+ *              group. It will have all attributes and behaviors of a single
+ *              group. It will be able to access the database to get said
+ *              attributes of a group.
+ *
+ * Attributes: name     - The name of the group.
+ *             schedule - an array of days where each day is an array of events
+ *                        where each event has:
+ *                              name: string
+ *                              startTime: time
+ *                              endTime: time
+ *                              startDate: date
+ *                              endDate: date
+ *                              recurring: boolean
+ *             groupID     - unique ID of the group
+ *             membersList - a list of members with the group leader in 
+ *                           position zero. Each element will be a user email.
+ *             groupLeader - can manipulate information.
+ *
+ * Behaviors: createMeeting() - anyone will be able to create a meeting
+ *            freeTime()      - this will be our main algorithm to get the 
+ *                              available free time for our group. This will be
+ *                              delegated to a function outside of controller.
+ *            deleteGroup()   - delete this group
+ *            addMembers()    - add members to the group (only group leader)
+ *          
  * 
  */
 
  var currentUser = Parse.User.current();
 
 app.controller('GroupController', ['$scope', function($scope) { 
-   // $scope.title = 'This Month\'s Bestsellers'; 
-   $scope.companyName = 'HIPSTERinc';
-   $scope.userParticipatedGroups = currentUser.get("Groups");
-   $scope.numberOfGroups = $scope.userParticipatedGroups.length;
+   // $scope.title = 'This Month\'s Bestsellers'; TEMPLATE for attributes
 
-   $scope.addGroup = function(){
-    $scope.userParticipatedGroups[$scope.numberOfGroups] = $scope.numberOfGroups;
-    $scope.numberOfGroups++;
-    currentUser.set("Groups", $scope.userParticipatedGroups);
-    currentUser.save(null, {
-  		success: function(user) {}
-	});
-}
   
 }]);
--- public/js/controllers/ProfileController.js
+++ public/js/controllers/ProfileController.js
@@ -1,6 +1,24 @@
 /*
- * ProfileController.js
- * Controls the view when the user viewing their profile
+ * FileName: ProfileController.js
+ * Authors: Joe d'Eon (if you edit this file, add your name to this list)
+ * Description: This controller will be used to control the main profile view
+ *              of a user. It will have all the attributes and behaviors of
+ *              a single user defined, and will be able to access the database
+ *              to get said attributes.
+ *
+ * Attributes: name      - The name of the user.
+ *             email     - The user's unique email
+ *             groupList - A list of the groups a user is in.
+ *
+ * Behaviors:  logout() - log out
+ *             createGroup() - create a new group
+ *             createEvent() - create an event that is either singular or
+ *                             recurring in nature
+ *             deleteEvent() - delete an event
+ *             editEvent()   - edit an existing event
+ *
+ *          
+ * 
  */
 
 //Link to Parse database - accepts application_ID, JavaScript_Key
@@ -11,6 +29,53 @@ var currentUser = Parse.User.current();
 app.controller('ProfileController', ['$scope', function($scope) {
 	$scope.userName = currentUser.get("name");
 	$scope.joinDate = currentUser.createdAt;
+
+
+  $scope.userParticipatedGroups = currentUser.get("Groups");
+  $scope.numberOfGroups = $scope.userParticipatedGroups.length;
+
+  $scope.groupView = false;
+  $scope.profileView = true;
+
+  $scope.toggleGroupView = function(){
+    $scope.groupView = true;
+    $scope.profileView = false;
+  }
+
+  $scope.toggleProfileView = function(){
+    $scope.groupView = false;
+    $scope.profileView = true;
+  }
+   $scope.addGroup = function(){
+    $scope.userParticipatedGroups[$scope.numberOfGroups] = $scope.numberOfGroups;
+    $scope.numberOfGroups++;
+    currentUser.set("Groups", $scope.userParticipatedGroups);
+    currentUser.save(null, {
+  		success: function(user) {}
+	});
+}
+
+
+
+
+
+
+
+  /************************************************************************
+   * Name:		logout()
+
+   * Purpose:		Allows the user to logout.
+
+   * Called In:   main()
+
+   * Description:	Calls Parse's logout function. 
+   ************************************************************************/
+  $scope.logout = function(){
+
+    Parse.User.logOut();
+    location.href='login/login.html';
+
+  }
 
     
 }]);
