cd41895bddf83145a79966e866109974947b3e59
Stephen Gilardi <sgilardi8@hotmail.com>
2015-05-06 13:24:38 -0700

    Implementation of adding groups is correct. Add additional comments to ProfileController

--- public/js/controllers/ProfileController.js
+++ public/js/controllers/ProfileController.js
@@ -6,16 +6,26 @@
  *              a single user defined, and will be able to access the database
  *              to get said attributes.
  *
- * Attributes: name      - The name of the user.
+ * Attributes: userName  - The name of the user.
  *             email     - The user's unique email
+ *             joinDate  - The date the user signed up.   
+ *             userParticipatedGroups - The array containing the list of groups the currentUser is part of.
+ *             numberOfGroups - The number of groups the current user is part of.            
  *             groupList - A list of the groups a user is in.
+ *             groupView - Determines if the group element in the HTML is shown.
+ *             profileView - Determines if the profile element in the HTML is shown.
+ *             newFriendEmail - The email of the friend being added to a group.
+ *
  *
  * Behaviors:  logout() - log out
- *             createGroup() - create a new group
+ *             addGroup() - create a new group
  *             createEvent() - create an event that is either singular or
  *                             recurring in nature
  *             deleteEvent() - delete an event
  *             editEvent()   - edit an existing event
+ *             toggleGroupView() - Selects only the Group view to be shown
+ *             toggleProfileView() - Selects only the Profile view to be shown
+ *             removeAllGroups() - Removes the current user from all groups.
  *
  *          
  * 
@@ -30,23 +40,22 @@ var currentUser = Parse.User.current();
 app.controller('ProfileController', ['$scope', function($scope) {
   $scope.sched = new Schedule(); // will be changed to pull schedule down
 	$scope.userName = currentUser.get("name");
-  $scope.userID = currentUser.id
 	$scope.joinDate = currentUser.createdAt;
+  $scope.email = currentUser.get("email");
 
 
   $scope.userParticipatedGroups = [];
   $scope.numberOfGroups = $scope.userParticipatedGroups.length;
- var GroupList = Parse.Object.extend("GroupList");
- var query = new Parse.Query(GroupList);
- console.log("First query for init");
- console.log(query);
- console.log(GroupList);
- query.get($scope.userID, {
- success: function(object) {
-    console.log(object);
-    $scope.userParticipatedGroups = object._serverData.userGroups;
+  
+  //This Query's the database for the CURRENT users GroupList Object.
+  //Once it finds the GroupList object, it pulls down the array and fills it locally.
+  var GroupList = Parse.Object.extend("GroupList");
+  var query = new Parse.Query(GroupList);
+  query.equalTo("userEmail", $scope.email);
+  query.find({
+  success: function(object) {
+    $scope.userParticipatedGroups = object[0]._serverData.userGroups;
     $scope.numberOfGroups = $scope.userParticipatedGroups.length;
-    console.log($scope.userParticipatedGroups);
     },
     error: function(object, error) {
     }
@@ -58,30 +67,58 @@ app.controller('ProfileController', ['$scope', function($scope) {
 
   $scope.newFriendEmail = "";
 
+  /************************************************************************
+   * Name:    toggleGroupView()
+
+   * Purpose:   Allows the user to see the Group view.
+
+   * Called In:   index.html
+
+   * Description: Shows and Hides specific elements in index.html to control what is shown.
+   ************************************************************************/
   $scope.toggleGroupView = function(){
     $scope.groupView = true;
     $scope.profileView = false;
   }
 
+  /************************************************************************
+   * Name:    toggleProfileView()
+
+   * Purpose:   Allows the user to see the Profile view.
+
+   * Called In:   index.html
+
+   * Description: Shows and Hides specific elements in index.html to control what is shown.
+   ************************************************************************/
   $scope.toggleProfileView = function(){
     $scope.groupView = false;
     $scope.profileView = true;
   }
   
+  /************************************************************************
+   * Name:    removeAllGroups()
 
+   * Purpose:   Allows the user to remove themselves from all groups they are in.
+
+   * Called In:   index.html
+
+   * Description: Removs all groups found in their GroupList userGroups array.
+   ************************************************************************/
   $scope.removeAllGroups = function(){
+    $scope.userParticipatedGroups = []; //would really be the ID;
     $scope.numberOfGroups = 0;
-    currentUser.set("Groups", [] );
-    currentUser.save(null, {
-      success: function(user) {}
+
+    var query = new Parse.Query(GroupList);
+    query.equalTo("userEmail", $scope.email)
+    query.find({
+      success: function(object) {
+      object[0].set("userGroups", $scope.userParticipatedGroups);
+      object[0].save();
+      },
+      error: function(object, error) {
+      }
     });
   }
-
-
-
-
-
-
 
   /************************************************************************
    * Name:		logout()
@@ -100,30 +137,38 @@ app.controller('ProfileController', ['$scope', function($scope) {
 
   }
 
+  /************************************************************************
+   * Name:    addGroup()
 
+   * Purpose:   Allows the user to create a group and include an email to invite to the group.
+
+   * Called In:   index.html
+
+   * Description: Creates a new group, and adds the new group to the GroupList userGroups array for both the current user the and user they have selected.
+   ************************************************************************/
    $scope.addGroup = function(){
     $scope.userParticipatedGroups[$scope.numberOfGroups] = $scope.newGroupName //would really be the ID;
     $scope.numberOfGroups++;
 
-    var GroupList = Parse.Object.extend("GroupList");
+    //This sets the current User's GroupList userGroups array to be updated with the new group
     var query = new Parse.Query(GroupList);
-    query.get($scope.userID, {
+    query.equalTo("userEmail", $scope.email)
+    query.find({
       success: function(object) {
       console.log("good refresh");
-      object.set("userGroups", $scope.userParticipatedGroups);
-      object.save();
+      object[0].set("userGroups", $scope.userParticipatedGroups);
+      object[0].save();
       },
       error: function(object, error) {
       }
     });
-
+    
+    //This sets the friend User's GroupList userGroups array to be updated with the new group
+    var query = new Parse.Query(GroupList);
     query.equalTo("userEmail", $scope.newFriendEmail);
-    console.log(query);
-    console.log($scope.newFriendEmail)
     query.find({
       success: function(object) {
         console.log("good refresh for friend");
-        console.log(object[0]);
         var tempList = object[0]._serverData.userGroups;
         tempList[tempList.length] = $scope.newGroupName;
         object[0].set("userGroups", tempList);
