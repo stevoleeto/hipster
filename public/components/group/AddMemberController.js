d8a5d378d42b4a6bad6433fde5167232b5db1df9
Saveen Chadalawada <sachadal@ucsd.edu>
2015-05-30 15:54:19 -0700

    Add members by clicking friends in friendlist

--- public/components/group/AddMemberController.js
+++ public/components/group/AddMemberController.js
@@ -1,7 +1,11 @@
 app.controller('AddMemberController', function($scope, $modalInstance, friendList){
 
 	$scope.friends = friendList;
-	
+
+	$scope.setEmail = function(email) {
+		$scope.newMemberEmail = email;
+	}
+
 	$scope.save = function(){
 		$modalInstance.close($scope.newMemberEmail);
 	}
