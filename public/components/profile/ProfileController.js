8a7d765fdeefddc6808ea9544e62c58407cc2a32
Stephen Gilardi <sgilardi8@hotmail.com>
2015-05-30 21:06:17 -0700

    change group color

--- public/components/profile/ProfileController.js
+++ public/components/profile/ProfileController.js
@@ -84,10 +84,10 @@ app.controller('ProfileController', ['$scope', 'groupService', 'eventService', '
                 });
             };
 
-            $scope.editGroupModal = function (oldName, oldColor) {
+            $scope.editGroupModal = function (oldName, oldColor, oldID) {
 
-                openModal('editGroup.html', 'EditGroupController', 'lg', {name: oldName, color: oldColor}).then(function (newGroupSettings) {
-                    console.log(newGroupSettings);
+                openModal('editGroup.html', 'EditGroupController', 'lg', {name: oldName, color: oldColor, id:oldID}).then(function (editedGroup) {
+                    editGroup(editedGroup.id,editedGroup.newColor);
                 }, function () {
                     $scope.myGroupList = userService.getNewGroupList();
                 });
@@ -229,6 +229,22 @@ $scope.addGroup = function(){
     groupService.setGroupId($scope.currentGroupId);
     groupService.setGroupColor($scope.currentGroupColor);
 }
+
+var editGroup = function(groupID, newColor){
+    for(index = 0; index < $scope.myGroupList.length; index++){
+        if(groupID === $scope.myGroupList[index]['id']){
+            console.log("Found the group!");
+            $scope.myGroupList[index]['color'] = newColor;
+            break;
+        }
+    }
+
+    dataBaseService.queryGroupList($scope.email).then(function(groupListQuery){
+        groupListQuery[0].set("userGroups", $scope.myGroupList);
+        groupListQuery[0].save();
+    });
+}
+
 
 $scope.updateSingleGroupTab = function(name){
     $scope.singleGroupName = name;
