04b25dbfc152f291ea012ecd25de6cdfbf3fc9b6
Saveen Chadalawada <sachadal@ucsd.edu>
2015-05-31 01:34:37 -0700

    Aligned create event text fields

---
+++ public/components/profile/ContactController.js
@@ -0,0 +1,26 @@
+app.controller('ContactController', function($scope, $modalInstance, modalParams){
+
+	$scope.name = modalParams.name;
+	$scope.email = modalParams.email;
+
+	$scope.fillBody = function() {
+		$scope.message = "Good afternoon,\r\n\r\nI was perusing through your site and I just wanted say that it is " + 
+						 "absolutely amazing. It is by far the best website of all time to exist on the internet." + 
+						 " Props to all the members who contributed to this site.\r\n\r\nThank you for being awesome! :D";
+	}
+
+	$scope.ok = function() {
+		console.log("send clicked!");
+		
+        Parse.Cloud.run('mailGroupAlert', {email: "saveenchad@yahoo.com", group: "hello world"}, {
+            success: function(result) {console.log("SUCCESS");console.log(result);},
+            error: function(error) {console.log("ERROR");console.log(error);}
+        });
+
+        console.log("email sent");
+	}
+
+	$scope.cancel = function(){
+		$modalInstance.dismiss('cancel');
+	}
+});
