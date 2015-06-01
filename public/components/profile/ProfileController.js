12a07c3787079212f67fc80d0b6b913e700c529a
jdeon <jmdeon@gmail.com>
2015-06-01 10:52:03 -0700

    remove extraneous contoller from PC

--- public/components/profile/ProfileController.js
+++ public/components/profile/ProfileController.js
@@ -135,7 +135,7 @@ app.controller('ProfileController', ['$scope', 'groupService', 'eventService', '
 
             $scope.contactModal = function () {
                 openModal('contact.html', 'ContactController', 'lg', {name: $scope.userName, email: $scope.email}).then(function (){
-                    
+
                 }, function (){
                     $scope.myGroupList = userService.getNewGroupList();
                 });
@@ -185,7 +185,7 @@ app.controller('ProfileController', ['$scope', 'groupService', 'eventService', '
                         }
                     })
                     /* GOOGLE CALENDAR */
-                    
+
                     if(currentUser.get("googleCalendarID")){ // if user has calID
                         userService.setGoogleCalendar(currentUser.get("googleCalendarID")).then(function(){
                             var newCalendar = userService.getGoogleCalendar();
@@ -546,32 +546,3 @@ app.controller('ProfileController', ['$scope', 'groupService', 'eventService', '
             };
 
         }]);//end profilecontrller
-
-
-
-app.controller('ContactController', function($scope, $modalInstance, modalParams){
-
-    $scope.name = modalParams.name;
-    $scope.email = modalParams.email;
-
-    $scope.fillBody = function() {
-        $scope.message = "Good afternoon,\r\n\r\nI was perusing through your site and I just wanted say that it is " + 
-                         "absolutely amazing. It is by far the best website of all time to exist on the internet." + 
-                         " Props to all the members who contributed to this site.\r\n\r\nThank you for being awesome! :D";
-    }
-
-    $scope.ok = function() {
-        console.log("send clicked!");
-        
-        Parse.Cloud.run('mailContactUs', {name: $scope.name, email: $scope.name, message: $scope.message}, {
-            success: function(result) {console.log("SUCCESS");console.log(result);},
-            error: function(error) {console.log("ERROR");console.log(error);}
-        });
-
-        console.log("email sent");
-    }
-
-    $scope.cancel = function(){
-        $modalInstance.dismiss('cancel');
-    }
-});
