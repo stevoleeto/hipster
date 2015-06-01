4e6d7fa1467385de6ac92f10ea3610d48492ab20
Saveen Chadalawada <sachadal@ucsd.edu>
2015-05-31 17:36:14 -0700

    Lot of emailing

--- cloud/main.js
+++ cloud/main.js
@@ -44,6 +44,29 @@ Parse.Cloud.define("mailContactUs", function(request, response){
 	});
 });
 
+Parse.Cloud.define("mailNewGroupEvent", function(request, response){
+	console.log(request.params);
+	var Mailgun = require('mailgun');
+	Mailgun.initialize('sandboxba98e37f8db04e8fa51e87a9352aec44.mailgun.org', 'key-f72d9fd43656a1032df21d7caa7865e0');
+
+	Mailgun.sendEmail({
+	  to: request.params.members,
+	  from: "BusyBlocks@hipster.com",
+	  subject: "New Event Created!",
+	  html: "<p>" + request.params.user + " created a new event called '" + request.params.eventName + "' in the group '" 
+	  		+ request.params.group + "'.</p><b><u>Details</u></b><br>" + request.params.details +  "<br><br><p>Check it out at <a href='hipster.parseapp.com'>hipster.parseapp.com</a></p>"},
+	  {
+	  success: function(httpResponse) {
+	    console.log(httpResponse);
+	    response.success("Email sent!");
+	  },
+	  error: function(httpResponse) {
+	    console.error(httpResponse);
+	    response.error("Uh oh, something went wrong");
+	  }
+	});
+});
+
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
--- public/components/profile/ContactController.js
+++ public/components/profile/ContactController.js
@@ -3,17 +3,13 @@ app.controller('ContactController', function($scope, $modalInstance, modalParams
 	$scope.name = modalParams.name;
 	$scope.email = modalParams.email;
 
-	$scope.fillBody = function() {
-		$scope.message = "Good afternoon,\r\n\r\nI was perusing through your beautiful site and I just wanted say that it is " + 
-						 "absolutely amazing. It is by far the best website of all time to exist on the internet." + 
-						 " Props to all the members who contributed to this site.\r\n\r\nThank you for being awesome! :D";
-	}
-
-	$scope.ok = function() {		
+	$scope.ok = function() {	
         Parse.Cloud.run('mailContactUs', {name: $scope.name, email: $scope.email, message: $scope.message}, {
             success: function(result) {},
             error: function(error) {}
         });
+
+        $modalInstance.dismiss('cancel');
 	}
 
 	$scope.cancel = function(){
