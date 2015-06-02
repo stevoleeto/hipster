c6d32de36458d2e9611d481813cf58b7319d2bc0
Saveen Chadalawada <sachadal@ucsd.edu>
2015-06-01 20:36:23 -0700

    Fixed repDays message showing under stuff

---
+++ public/components/login/scroll.js
@@ -0,0 +1,66 @@
+/**
+    Smoothly scroll element to the given target (element.scrollTop)
+    for the given duration
+
+    Returns a promise that's fulfilled when done, or rejected if
+    interrupted
+ */
+var smooth_scroll_to = function() {
+    var DURATION = 1000;
+
+    var start_time = Date.now();
+    var end_time = start_time + DURATION;
+
+    var start_top = document.body.scrollTop;
+    var distance = window.innerHeight - start_top;
+
+    // based on http://en.wikipedia.org/wiki/Smoothstep
+    var smooth_step = function(start, end, point) {
+        if(point <= start) { return 0; }
+        if(point >= end) { return 1; }
+        var x = (point - start) / (end - start); // interpolation
+        return x*x*(3 - 2*x);
+    }
+
+    return new Promise(function(resolve, reject) {
+        // This is to keep track of where the element's scrollTop is
+        // supposed to be, based on what we're doing
+        var previous_top = document.body.scrollTop;
+
+        // This is like a think function from a game loop
+        var scroll_frame = function() {
+            if(document.body.scrollTop != previous_top) {
+                reject("interrupted");
+                return;
+            }
+
+            // set the scrollTop for this frame
+            var now = Date.now();
+            var point = smooth_step(start_time, end_time, now);
+            var frameTop = Math.round(start_top + (distance * point));
+            document.body.scrollTop = frameTop;
+
+            // check if we're done!
+            if(now >= end_time) {
+                resolve();
+                return;
+            }
+
+            // If we were supposed to scroll but didn't, then we
+            // probably hit the limit, so consider it done; not
+            // interrupted.
+            if(document.body.scrollTop === previous_top
+                && document.body.scrollTop !== frameTop) {
+                resolve();
+                return;
+            }
+            previous_top = document.body.scrollTop;
+
+            // schedule next frame for execution
+            setTimeout(scroll_frame, 0);
+        }
+
+        // boostrap the animation process
+        setTimeout(scroll_frame, 0);
+    });
+}
