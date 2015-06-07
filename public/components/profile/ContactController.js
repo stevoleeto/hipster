224d66dbf2e7c3025866a2068955acfd017459b5
Saveen Chadalawada <sachadal@ucsd.edu>
2015-06-07 00:25:20 -0700

    Documented ContactController.js

--- public/components/profile/ContactController.js
+++ public/components/profile/ContactController.js
@@ -1,23 +1,30 @@
 app.controller('ContactController', function($scope, $modalInstance, modalParams){
 
+	// grab the data passed in from modalParams
 	$scope.name = modalParams.name;
 	$scope.email = modalParams.email;
 
+	// When the user clicks the ok button in the modal
 	$scope.ok = function() {
+		// Check if the user didn't type anything into the message field
 		if ($scope.message != "" && $scope.message != undefined) {
+			// Email the dev team ( Joe, Steven, Mikey, Saveen ) the user's feedback
 			Parse.Cloud.run('mailContactUs', {name: $scope.name, email: $scope.email, message: $scope.message}, {
 	            success: function(result) {},
 	            error: function(error) {}
 	        });
 
+			// Notify the user that their message was sent
 	        $scope.sent = true;
 
 	        setTimeout(function(){
                 $scope.sent = false;
             }, 2000);
 
+	        // Reset the message field
             $scope.message = "";
 		} else {
+			// if the user didn't enter anything, notify user with an error
 			$scope.error = true;
 
 	        setTimeout(function(){
@@ -26,7 +33,9 @@ app.controller('ContactController', function($scope, $modalInstance, modalParams
 		}
 	}
 
+	// If the user clicked the cancel button on the modal
 	$scope.cancel = function(){
+		// Close the modal.
 		$modalInstance.dismiss('cancel');
 	}
 });
